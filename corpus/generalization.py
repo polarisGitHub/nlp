279311fe4da5478e25e8dac4d751c8d36a72f1ab
polaris <polaris1478@gmail.com>
2018-02-03 01:43:53 +0800

    jieba不支持下划线组成词

--- corpus/generalization.py
+++ corpus/generalization.py
@@ -23,15 +23,15 @@ if options.output == "":
 # datetime
 # address
 regexes = {
-    re.compile("(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]"): "__URL__",
-    re.compile("\d{17}[\d|x]|\d{15}"): "__IDCARD__",
-    re.compile("jd_[\d\w/*]*"): "__PIN__",
-    re.compile("#E-s(\d)+"): "__STICKER__",
-    re.compile("(13|14|15|17|18|19)([0-9|*]){9}"): "__MOBILE__",
-    re.compile("^(0[0-9]{2,3}/-)?([2-9][0-9]{6,7})+(/-[0-9]{1,4})}"): "__PHONE__",
-    re.compile("[^_][a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+"): "__MAIL__",
-    re.compile("[a-zA-Z0-9_-|*]+@[a-zA-Z0-9_-|*]+"): "__MAIL__",
-    re.compile("[-]?[0-9]*\.?[0-9]+"): "__NUM__"
+    re.compile("(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]"): "URLTOKEN",
+    re.compile("\d{17}[\d|x]|\d{15}"): "IDCARDTOKEN",
+    re.compile("jd_[\d\w/*]*"): "PINTOKEN",
+    re.compile("#E-s(\d)+"): "STICKERTOKEN",
+    re.compile("(13|14|15|17|18|19)([0-9|*]){9}"): "MOBILETOKEN",
+    re.compile("^(0[0-9]{2,3}/-)?([2-9][0-9]{6,7})+(/-[0-9]{1,4})}"): "PHONETOKEN",
+    re.compile("[^_][a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+"): "EMAILTOKEN",
+    re.compile("[a-zA-Z0-9_-|*]+@[a-zA-Z0-9_-|*]+"): "EMAILTOKEN",
+    re.compile("[-]?[0-9]*\.?[0-9]+"): "NUMTOKEN"
 }
 
 
