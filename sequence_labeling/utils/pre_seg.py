6dafb4b9ff002fb993493d4bf917e56a47b07f70
polaris <polaris1478@gmail.com>
2018-03-24 23:50:29 +0800

    要求语料处理好全角

--- sequence_labeling/utils/pre_seg.py
+++ sequence_labeling/utils/pre_seg.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import re
-from . import full_half_converter
 
 
 # 初分词，按标点切分
 def process(sentence):
-    sentence = full_half_converter.convert(sentence,
-                                           full_half_converter.FH_ALPHA,
-                                           full_half_converter.FH_NUM,
-                                           full_half_converter.FH_SPACE)
     return list(filter(lambda sentence: sentence != "",
                        map(lambda sentence: sentence.strip(), re.split("[,，.。!！?？;；]", sentence))))
